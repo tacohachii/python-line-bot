d2723ddd1d543a5fe6090ea0914e1d2630d5baa2
tacohachi <zero0hati8tk8@gmail.com>
2020-04-28 03:14:38 +0900

    型変換

--- db_line.py
+++ db_line.py
@@ -13,29 +13,29 @@ def new_register(user_lineid):
 
 def reg_menu_name(user_lineid, menu_name):
     # 最新のユーザーを取ってきてメニュー名を入れている
-    cursor.execute("UPDATE recipe SET name = {0} WHERE id = (SELECT id FROM recipe WHERE user_id= {1} ORDER BY id)".format(menu_name, user_lineid))
+    cursor.execute("UPDATE recipe SET name = '{0}' WHERE id = (SELECT id FROM recipe WHERE user_id= '{1}' ORDER BY id)".format(menu_name, user_lineid))
     conn.commit()
     return 
     
 def reg_menu_recipe(user_lineid, menu_recipe):
-    cursor.execute("UPDATE recipe SET recipe = {0} WHERE id = (SELECT id FROM recipe WHERE user_id= {1} ORDER BY id)".format(menu_recipe, user_lineid))
+    cursor.execute("UPDATE recipe SET recipe = '{0}' WHERE id = (SELECT id FROM recipe WHERE user_id= '{1}' ORDER BY id)".format(menu_recipe, user_lineid))
     conn.commit()
     return 
 
 def db_search(user_lineid, menu_name):
-    cursor.execute("SELECT * FROM recipe WHERE user_id= {0} and name= {1}".format(user_lineid, menu_name))
+    cursor.execute("SELECT * FROM recipe WHERE user_id= '{0}' and name= '{1}'".format(user_lineid, menu_name))
     hoge = cursor.fetchall()
     return hoge[0][0]
 
 def find_latest_value(user_lineid, message, column):
     if column == 'user_id':
-        cursor.execute("SELECT * FROM recipe WHERE user_id= {0}".format(user_lineid))
+        cursor.execute("SELECT * FROM recipe WHERE user_id= '{0}'".format(user_lineid))
     if column == 'name':
         menu_name = message
-        cursor.execute("SELECT * FROM recipe WHERE name = {0} and id = (SELECT id FROM recipe WHERE user_id= {1} ORDER BY id)".format(menu_name, user_lineid))
+        cursor.execute("SELECT * FROM recipe WHERE name = '{0}' and id = (SELECT id FROM recipe WHERE user_id= '{1}' ORDER BY id)".format(menu_name, user_lineid))
     if column == 'recipe':
         menu_recipe = message
-        cursor.execute("SELECT * FROM recipe WHERE recipe = {0} and id = (SELECT id FROM recipe WHERE user_id= {1} ORDER BY id)".format(menu_recipe, user_lineid))
+        cursor.execute("SELECT * FROM recipe WHERE recipe = '{0}' and id = (SELECT id FROM recipe WHERE user_id= '{1}' ORDER BY id)".format(menu_recipe, user_lineid))
     
     hoge = cursor.fetchall()
     if len(hoge) == 0:
