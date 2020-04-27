d0b3e14eb5ad1977dae78549c89095e4e9fec0ad
tacohachi <zero0hati8tk8@gmail.com>
2020-04-28 03:39:54 +0900

    更新

--- db_line.py
+++ db_line.py
@@ -27,15 +27,15 @@ def db_search(user_lineid, menu_name):
     hoge = cursor.fetchall()
     return hoge[0][0]
 
-def find_latest_value(user_lineid, message, column):
+def find_latest_value(user_lineid, column):
     if column == 'user_id':
         cursor.execute("SELECT * FROM recipe WHERE user_id= '{0}'".format(user_lineid))
     if column == 'name':
-        menu_name = message
-        cursor.execute("SELECT * FROM recipe WHERE name = '{0}' and recipe IS NULL and id = (SELECT id FROM recipe WHERE user_id= '{1}' ORDER BY id)".format(menu_name, user_lineid))
+        # ユーザーの最新の情報から，nameが空でrecipeが空のものを抜き出す
+        cursor.execute("SELECT * FROM recipe WHERE name = IS NULL and recipe IS NULL and id = (SELECT id FROM recipe WHERE user_id= '{1}' ORDER BY id)".format(user_lineid))
     if column == 'recipe':
-        menu_recipe = message
-        cursor.execute("SELECT * FROM recipe WHERE recipe = '{0}' and recipe IS NOT NULL and id = (SELECT id FROM recipe WHERE user_id= '{1}' ORDER BY id)".format(menu_recipe, user_lineid))
+        # ユーザーの最新の情報から，nameに値があってrecipeが空のものを抜き出す
+        cursor.execute("SELECT * FROM recipe WHERE recipe = IS NOT NULL and recipe IS NULL and id = (SELECT id FROM recipe WHERE user_id= '{1}' ORDER BY id)".format(user_lineid))
     
     hoge = cursor.fetchall()
     if len(hoge) == 0:
